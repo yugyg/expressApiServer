c90ac2e2f320a9b3ec253bc04df6a6d9f431d1c8
chenwei <33944940+cw0722@users.noreply.github.com>
2020-01-14 17:26:24 +0800

    物流配置修改

--- expressApiServer/src/main/java/com/yugyg/util/ConfigUtil.java
+++ expressApiServer/src/main/java/com/yugyg/util/ConfigUtil.java
@@ -5,35 +5,43 @@ import com.typesafe.config.ConfigFactory;
 
 /**
  * 读取application.conf 配置文件,application.conf需要在classpath路径下面
- * 
- * @author sunning
  *
+ * @author sunning
  */
 public class ConfigUtil {
 
-	private static final String redisIp = "redisIp";
-	private static final String redisPort = "redisPort";
+    private static final String redisIp = "redisIp";
+    private static final String redisPort = "redisPort";
+    private static final String redisUserName = "redisUserName";
+    private static final String redisPassword = "redisPassword";
 
-	private static Config conf;
+    private static Config conf;
 
-	static {
-		conf = ConfigFactory.load();
-	}
+    static {
+        conf = ConfigFactory.load();
+    }
 
-	public static String getStringConfigProperties(String name) {
-		return conf.getString(name);
-	}
+    public static String getStringConfigProperties(String name) {
+        return conf.getString(name);
+    }
 
-	public static int getIntConfigProperties(String name) {
-		return conf.getInt(name);
-	}
+    public static int getIntConfigProperties(String name) {
+        return conf.getInt(name);
+    }
 
-	public static String getRedisIp() {
-		return conf.getString(redisIp);
-	}
+    public static String getRedisIp() {
+        return conf.getString(redisIp);
+    }
 
-	public static int getRedisPort() {
-		return conf.getInt(redisPort);
-	}
+    public static int getRedisPort() {
+        return conf.getInt(redisPort);
+    }
 
+    public static String getRedisUserName() {
+        return conf.getString(redisUserName);
+    }
+
+    public static String getRedisPassword() {
+        return conf.getString(redisPassword);
+    }
 }
--- expressApiServer/src/main/java/com/yugyg/util/RedisUtil.java
+++ expressApiServer/src/main/java/com/yugyg/util/RedisUtil.java
@@ -6,42 +6,55 @@ import redis.clients.jedis.JedisPoolConfig;
 
 public class RedisUtil {
 
-	private static JedisPool pool = null;
-	static {
+    private static JedisPool pool = null;
 
-		pool = new JedisPool(new JedisPoolConfig(), ConfigUtil.getRedisIp(), ConfigUtil.getRedisPort());
-	}
+    static {
+        System.out.println(ConfigUtil.getRedisPassword());
+        if (ConfigUtil.getRedisPassword() != null && !ConfigUtil.getRedisPassword().isEmpty()) {
+            pool = new JedisPool(new JedisPoolConfig(), ConfigUtil.getRedisIp(), ConfigUtil.getRedisPort(), 0,
+                ConfigUtil.getRedisPassword(),0,ConfigUtil.getRedisUserName());
+        } else {
+            pool = new JedisPool(new JedisPoolConfig(), ConfigUtil.getRedisIp(), ConfigUtil.getRedisPort());
+        }
 
-	public static Jedis getJedis() {
-		return pool.getResource();
-	}
-	
-	public static String  getJedisPara(String key) {
-		Jedis jedis = null;
-		String para = "";
-		try {
-			jedis = getJedis();
-			para = jedis.get(key);
-		} finally {
-			 if (jedis != null) {
-               jedis.close();
-             }
-		}
-		return para;
-	}
-	
-	public static String  setJedisPara(String key,String value) {
-		Jedis jedis = null;
-		String para = "";
-		try {
-			jedis = getJedis();
-			para = jedis.set(key,value);
-		} finally {
-			if (jedis != null) {
-				jedis.close();
-			}
-		}
-		return para;
-	}
-	
+    }
+
+    public static Jedis getJedis() {
+        return pool.getResource();
+    }
+
+    public static String getJedisPara(String key) {
+        Jedis jedis = null;
+        String para = "";
+        try {
+            jedis = getJedis();
+            para = jedis.get(key);
+        } finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+        return para;
+    }
+
+    public static String setJedisPara(String key, String value) {
+        Jedis jedis = null;
+        String para = "";
+        try {
+            jedis = getJedis();
+            para = jedis.set(key, value);
+        } finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+        return para;
+    }
+
+    public static void main(String[] args) {
+//        pool = new JedisPool(new JedisPoolConfig(), "prod-cn-rds.redis.rds.aliyuncs.com", 6379, 0, "12yH35JY!vvx","");
+
+        setJedisPara("t_1","test");
+        System.out.println(getJedisPara("t_1"));;
+    }
 }
